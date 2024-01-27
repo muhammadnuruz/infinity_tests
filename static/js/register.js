7ad3790940c09196c05e64808dbdcf8636c276b1
muhammadnur <muhammadnurpdp@gmail.com>
2024-01-27 20:03:25 +0500

    update requirements.txt

--- static/js/register.js
+++ static/js/register.js
@@ -15,7 +15,7 @@ document.getElementById('registerForm').addEventListener('submit', function(e) {
     .then(data => {
         if (data.password) {
             // Ro'yxatdan o'tish muvaffaqiyatli bo'lsa, bosh sahifaga yo'naltirish
-            window.location.href = 'file:///home/muhammadnur/VisualCodeProjects/index.html';
+            window.location.href = 'http://127.0.0.1:8000/';
         } else {
             // Xabar ko'rsatish
             document.getElementById('message').textContent = data.full_name;
