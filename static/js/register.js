15e62e6aa8361a8937e9c242e335aae3e8f83e42
muhammadnur <muhammadnurpdp@gmail.com>
2024-01-27 18:39:08 +0500

    update requirements.txt

--- static/js/register.js
+++ static/js/register.js
@@ -4,7 +4,7 @@ document.getElementById('registerForm').addEventListener('submit', function(e) {
     const fullName = document.getElementById('fullName').value;
     const password = document.getElementById('password').value;
 
-    fetch('http://localhost:8000/api/users/create/', {
+    fetch('http://16.171.162.86:8000//api/users/create/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
