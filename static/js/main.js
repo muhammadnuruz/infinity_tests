3894b27ad8e12897b6a4c6c65e100de7f7c56f0f
muhammadnur <muhammadnurpdp@gmail.com>
2024-02-03 14:42:34 +0500

    update requirements.txt

--- static/js/main.js
+++ static/js/main.js
@@ -101,6 +101,9 @@ function handleQuestionsAndAnswers(data) {
         };
         qAndADiv.appendChild(button);
     });
+
+    // Test tugmasini chiqarish
+    document.getElementById('endTest').classList.remove('hide');
 }
 
 function submitAnswer(answer) {
@@ -202,3 +205,21 @@ correctAnswersElement.textContent = `To'g'ri javoblar: ${data.correct_questions}
 const wrongAnswersElement = document.createElement('li');
 wrongAnswersElement.className = 'wrong';
 wrongAnswersElement.textContent = `Noto'g'ri javoblar: ${data.wrong_questions}`;
+
+function endTest() {
+    fetch('/api/tests/end_test/', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        showTestResults(data);
+    })
+    .catch(error => {
+        console.error('Xato:', error);
+        document.getElementById('submitResponse').innerHTML = 'Xato: ' + error;
+    });
+}
